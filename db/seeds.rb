1dd4c61316b2f3fcd70007c89ee3b4c1ade66d16
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-06-09 19:44:08 -0500

    Updated seeds.rb to create products and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,46 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-require "csv"
-require "faker"
+#require "faker"
 
-676.times do
+#676.times do
+#    Product.create(
+#        title: Faker::Commerce.product_name,
+#        price: Faker::Commerce.price,
+#        stock_quantity: Faker::Number.between(from: 1, to: 100)
+#    )
+#end
+
+
+require "csv"
+
+# clear existing data
+Product.destroy_all
+Category.destroy_all
+
+# Set path to csv
+csv_file = Rails.root.join('db/products.csv')
+
+# Read the csv
+csv_data = File.read(csv_file)
+
+# Parse the csv
+products = CSV.parse(csv_data, headers: true)
+
+# Loop through each product
+products.each do |product|
+    category_name = product['category_name']
+
+    category = Category.find_or_create_by(name: category_name)
+
     Product.create(
-        title: Faker::Commerce.product_name,
-        price: Faker::Commerce.price,
-        stock_quantity: Faker::Number.between(from: 1, to: 100)
+        title: product['name'],
+        price: product['price'],
+        description: product['description'],
+        stock_quantity: ['stock quantity'],
+        category: category
     )
 end
+
+puts "Created #{Category.count} categories."
+puts "Created #{Product.count} products."
